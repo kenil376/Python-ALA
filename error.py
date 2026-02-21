dc70fe45d136008e59c3918d28f079cbaac057ed
kenil376 <kenilpatel230@gmail.com>
2026-02-21 17:29:47 +0530

    Update error.py

--- error.py
+++ error.py
@@ -1,1 +1,18 @@
+PROGRAM 22 — | Enrolment: 241105060102
 
+print("Factorial Calculator")
+num = int(input("Enter number: "))
+fact = 1
+i = 1
+while i <= num:
+   fact = fact * i
+   i = i + 1
+print("Factorial:", fact)
+if fact > 1000
+   print("Large factorial")
+else:
+   print("Small factorial")
+if num < 0:
+   print("Invalid input"
+else:
+   print("Valid input")
